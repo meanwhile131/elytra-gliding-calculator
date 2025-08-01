764f9813c1ea96ab4c2566a09505b73e1e9f9104
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-08-01 18:47:51 +0300

    add plotting of angle to distance

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <implementation.cpp>
+#include <matplot/matplot.h>
+#include <vector>
+using namespace std;
 
 int main()
 {
+    Vec3d intial_position = Vec3d(0, 150, 0);
     double bestDistance;
     float bestPitch;
-    Vec3d intial_position = Vec3d(0, 150, 0);
-    for (float pitch = -90; pitch < 90; pitch += 0.01)
+    vector<double> pitches;
+    vector<double> distances;
+    ofstream log("out.txt");
+    for (float pitch = -20; pitch < 20; pitch += 0.01)
     {
         GlidingPlayer player(pitch, intial_position);
         while (!player.simulateTick())
@@ -18,8 +24,19 @@ int main()
             bestDistance = distance;
             bestPitch = pitch;
         }
-        std::cout << distance << " for " << pitch << "\n";
+        pitches.push_back(pitch);
+        distances.push_back(distance);
+        log << "Pitch: " << pitch << " Distance:" << distance << "\n";
     }
-    std::cout << "Best: " << bestDistance << " for " << bestPitch << "\n";
+    log << "Best pitch is " << bestPitch << " for " << bestDistance << "\n";
+    log.close();
+    {
+        using namespace matplot;
+        auto f = figure(true);
+        auto ax = f->current_axes();
+        auto p = ax->plot(pitches, distances);
+        f->save("graph.png");
+        f->show();
+    }
     return 0;
 }
