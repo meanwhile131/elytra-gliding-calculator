dfadc5ac50ede7c70cd07da4c4ae0bc42b82f82e
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-08-02 12:22:27 +0300

    format code

--- src/dynamic_angle.cpp
+++ src/dynamic_angle.cpp
@@ -12,10 +12,11 @@ int main()
     vector<double> pitches;
     vector<double> distances;
     ofstream log("out.txt");
-    
+
     Vec3d position(0, 100, 0);
     Vec3d velocity(0, 0, 0);
-    while (position.z < 2000 && position.y > 0) {
+    while (position.z < 2000 && position.y > 0)
+    {
         Vec3d best_velocity(0, 0, 0);
         float best_pitch;
         for (float pitch = -90; pitch < 90; pitch += 0.01)
@@ -30,7 +31,8 @@ int main()
         velocity = best_velocity;
         position = position.add(best_velocity.x, best_velocity.y, best_velocity.z);
         log << position << " " << velocity << " " << best_pitch << "\n";
-        cout << position << " " << velocity << " " << best_pitch << "\n";;
+        cout << position << " " << velocity << " " << best_pitch << "\n";
+        ;
     }
     log.close();
     return 0;
--- src/two_angles.cpp
+++ src/two_angles.cpp
@@ -2,7 +2,6 @@
 #include <implementation.cpp>
 #include <vector>
 #include <fstream>
-#include <optional>
 using namespace std;
 
 int main()
@@ -12,23 +11,29 @@ int main()
     double bestDistance = -INFINITY;
     float best_pitch_up{};
     float best_pitch_down{};
-    for (float pitch_down = 0; pitch_down < 90; pitch_down++) {
-        for (float pitch_up = -90; pitch_up < 0; pitch_up++) {
+    for (float pitch_down = 0; pitch_down < 90; pitch_down++)
+    {
+        for (float pitch_up = -90; pitch_up < 0; pitch_up++)
+        {
             Vec3d position(0, 100, 0);
             Vec3d velocity(0, 0, 0);
             float pitch = 0;
-            while (position.y > 0) {
-                if (velocity.y < 0) {
+            while (position.y > 0)
+            {
+                if (velocity.y < 0)
+                {
                     pitch = pitch_down;
                 }
-                if (position.y < 5) {
+                if (position.y < 5)
+                {
                     pitch = pitch_up;
                 }
                 velocity = calcGlidingVelocity(velocity, pitch);
                 position += velocity;
             }
             log << "For " << pitch_down << "; " << pitch_up << ": " << position.z;
-            if (position.z > bestDistance) {
+            if (position.z > bestDistance)
+            {
                 best_pitch_up = pitch_up;
                 best_pitch_down = pitch_down;
                 bestDistance = position.z;
